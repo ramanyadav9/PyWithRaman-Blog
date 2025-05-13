382836d40357dec004fdf28bad09a85347a2ffb2
ramanyadav9 <9ramanyadav@gmail.com>
2025-05-13 17:25:26 +0530

    add readme

--- blog_webapp/blog_webapp/settings.py
+++ blog_webapp/blog_webapp/settings.py
@@ -142,8 +142,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dak99ine@gmail.com'
-EMAIL_HOST_PASSWORD = 'ocls whoh equm idtx'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your app password'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
